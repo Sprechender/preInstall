cf4f4dd1c51c4d1b7ddbe9f333eba0f190a3275b
Sprechender <150484926+Sprechender@users.noreply.github.com>
2023-12-10 21:04:53 +0100

    added tool / repo-downloader

--- main.py
+++ main.py
@@ -1,36 +1,101 @@
 import os
-import questionary
+import requests # shit to download stuff
+import sys # idk
+import questionary # questions
 
 def clean():
-    # For Windows
-    if os.name == 'nt':
-        _ = os.system('cls')
-
-    # For macOS and Linux
-    else:
-        _ = os.system('clear')
+        os.system('cls') # clear terminal
 
 def greet():
     print("WELCOME TO PREINSTALL!\n")
 
+def checkCompatibility():
+    if os.name != 'nt':
+        print("ERROR:\n    Is not Windows.\n    preInstall is only supported on Windows!")
+        exit()
+
 def initApp():
     clean() # clears the terminal
-    
     greet() # greets the user (ex. " welcome to preInstall")
+    checkCompatibility() # checks if system is compatible
     start() # starts IOHA menu
 
 def prepFunction():
     clean() # clears the terminal
 
 def aboutMenu():
-    print("Hey there! 👋 I'm Sprechender, the developer behind this python script.")
-    print("This Script helps you with the automated installion of multiple programs.")
-    aboutE = questionary.select(
+    print("Hey there! 👋 I'm Sprechender, the developer behind this python script.") # blablabla
+    print("This Script helps you with the automated installion of multiple programs.") # more blablabla
+    aboutE = questionary.select( # go back question
         message="Go back",
         instruction="(press enter)",
         choices=["back"],
     ).ask()
-    initApp()
+    
+    return; # return to main
+
+
+# config.preinstall
+
+def optionsMenu():
+    def loadOptionsMenu():
+        q__optionsMenu = questionary.select(
+            message="Select an option you want to edit",
+            choices=["Updates", "Default Repo", "Misc"],
+        ).ask()
+        
+        clean()
+    
+        if q__optionsMenu == "Updates":
+            q__optionsMenu__updates = questionary.select(
+                message="Do you want automatic updates",
+                choices=["Yes", "No"],
+            ).ask()
+            if q__optionsMenu__updates == "Yes":
+                print("true")
+            elif q__optionsMenu__updates == "No":
+                print("false")
+        
+        elif q__optionsMenu == "Default Repo":
+            q__optionsMenu__defaultRepo = questionary.select(
+                message="From which repo do you want to request instructions",
+                choices=["Official", "Untested", "Custom"],
+            ).ask()
+
+            if q__optionsMenu__defaultRepo == "Official":
+                print("official")
+            
+            elif q__optionsMenu__defaultRepo == "Untested":
+                print("untested")
+            
+            elif q__optionsMenu__defaultRepo == "Custom":
+                print("custom")
+            
+            print("default repo")
+
+        elif q__optionsMenu == "Misc":
+            q__optionsMenu__misc = questionary.select(
+                message="Some miscellaneous options",
+                choices=["Auto Install", "Export Instruction groups", "Logs", "Create config file"],
+            ).ask()
+            
+            if q__optionsMenu__misc == "Auto Install":
+                print("auto install")
+            
+            elif q__optionsMenu__misc == "Export Instruction groups":
+                print("export instruction groups")
+            
+            elif q__optionsMenu__misc == "Logs":
+                print("logs")
+            
+            elif q__optionsMenu__misc == "Create config file":
+                print("create config file")
+
+        print("misc")
+
+    loadOptionsMenu()
+
+    print("saving...")
 
 def installMenu():
     # brauche unbedingt besseren namen als instruction server
@@ -39,9 +104,9 @@ def installMenu():
         message="Select instruction file",
         choices=["Official (Online)", "Custom (Online)", "Local (Offline)"],
     ).ask()
-
+    
     if whichInstructionFileSys == "Official (Online)":
-        print("test")
+        print("apfel")
 
     if whichInstructionFileSys == "Custom (Online)":
         print(whichInstructionFileSys)
@@ -65,7 +130,8 @@ def start():
         # prepare Function
         prepFunction()
 
-        print("test")
+        optionsMenu()
+
         initApp()
 
     if startIOHA == "About":
@@ -74,6 +140,8 @@ def start():
 
         # call about function
         aboutMenu()
+
+        initApp() # reset
 
     if startIOHA == "Exit":
         # prepare Function
